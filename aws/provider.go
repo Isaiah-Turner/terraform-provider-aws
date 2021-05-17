e34e59bbe50ee986482caa5b119ef00f902853d4
Isaiah-Turner <42742035+Isaiah-Turner@users.noreply.github.com>
2021-05-17 16:25:33 -0400

    Added AppConfig Application Resource

--- aws/provider.go
+++ aws/provider.go
@@ -487,6 +487,7 @@ func Provider() *schema.Provider {
 			"aws_appautoscaling_target":                               resourceAwsAppautoscalingTarget(),
 			"aws_appautoscaling_policy":                               resourceAwsAppautoscalingPolicy(),
 			"aws_appautoscaling_scheduled_action":                     resourceAwsAppautoscalingScheduledAction(),
+			"aws_appconfig_application":                               resourceAwsAppconfigApplication(),
 			"aws_appmesh_gateway_route":                               resourceAwsAppmeshGatewayRoute(),
 			"aws_appmesh_mesh":                                        resourceAwsAppmeshMesh(),
 			"aws_appmesh_route":                                       resourceAwsAppmeshRoute(),
